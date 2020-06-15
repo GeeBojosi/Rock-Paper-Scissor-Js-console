27a3aee46ad7f6e81252135bfc02c97b0595bbbe
Gomolemo Bojosi <gee@geebojosi.com>
2020-06-15 18:46:27 +0200

    Added game reset functionality

--- game.js
+++ game.js
@@ -1,59 +1,57 @@
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let resetbtn = document.querySelector("#reset");
+let result = document.getElementById("result-msg");
+let userScore_span = document.querySelector("#player");
+let computerScore_span = document.querySelector("#computer");
 let userScore = 0;
 let computerScore = 0;
-let playerScore_span = document.getElementById('player-score');
-const computerScore_span = document.getElementById('computer-score');
-const rockButton = document.getElementById('rock');
-const paperButton = document.getElementById('paper');
-const scissorsButton = document.getElementById('scissors');
-const result = document.querySelector('.result');
-const matchEnd = 5;
 
-// get the Inputs from computer and player
+
+//Inputs from players
+
 function computerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomChoice = Math.floor(Math.random() * 3);
-    return choices[randomChoice];
+    let choices = ["rock", "paper", "scissors"];
+    let randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
 }
 
-function playerInput() {
-    rockButton.addEventListener('click', function() {
-        playRound("rock");
+function player() {
+    rock.addEventListener("click", function() {
+        Game("rock");
     });
-
-    paperButton.addEventListener('click', function() {
-        playRound("paper");
-    });
-
-    scissorsButton.addEventListener('click', function() {
-        playRound("scissors");
-    });
-}
-
-playerInput();
-
-//Functions for Win and lose and called within playRound function//
-
-// function win(){
-//     result.innerHTML = "You Win, Rock Crushes scissors";
-//     userScore++;
-//     playerScore_span.innerHTML = userScore;
-// }
-
-//Where gameplay happens
-function playRound(userChoice, computerSelection) {
-    computerSelection = computerChoice();
     
+    paper.addEventListener("click", function() {
+        Game("paper");
+    });
+    
+    scissors.addEventListener("click", function() {
+        Game("scissors");
+    });
+}
+
+resetbtn.addEventListener("click", function() {
+    reset();
+});
+
+player();
+
+
+function Game(userChoice, computerSelection) {
+    computerSelection = computerChoice();
+
     const win = `You Win, ${userChoice} beats ${computerSelection}`;
     const lose = `You Lose, ${computerSelection} beats ${userChoice}`;
     const draw = "it's a draw";
-    
+
     if(userChoice === computerSelection){
         result.innerHTML = draw;
     }else if(userChoice === "rock"){
         if(computerSelection === "scissors"){
             result.innerHTML = win;
             userScore++;
-            playerScore_span.innerHTML = userScore;
+            userScore_span.innerHTML = userScore;
         }else {
             result.innerHTML = lose;
             computerScore++;
@@ -63,7 +61,7 @@ function playRound(userChoice, computerSelection) {
         if(computerSelection === "rock"){
             result.innerHTML = win;
             userScore++;
-            playerScore_span.innerHTML = userScore;
+            userScore_span.innerHTML = userScore;
         }else {
             result.innerHTML = lose;
             computerScore++;
@@ -73,7 +71,7 @@ function playRound(userChoice, computerSelection) {
         if(computerSelection === "paper"){
             result.innerHTML = win;
             userScore++;
-            playerScore_span.innerHTML = userScore;
+            userScore_span.innerHTML = userScore;
         }else {
             result.innerHTML = lose;
             computerScore++;
@@ -81,35 +79,12 @@ function playRound(userChoice, computerSelection) {
         }
     }
 
-
-        
-    // }else if(userChoice === "paper"){
-    //     if(computerSelection === "rock"){
-    //         result.innerHTML = win;
-    //         userScore++;
-    //         playerScore_span.innerHTML = userScore;
-    //     }
-    // }else if(userChoice === "scissors"){
-    //     if(computerSelection === "paper"){
-    //         result.innerHTML = win;
-    //         userScore++;
-    //         playerScore_span = userScore;
-    //     }
-    // }
-    endGame();
 }
 
-function endGame() {
-
-    if(userScore >= matchEnd){
-        result.innerHTML = "You Win, You beat the computer";
-        return;
-    }
-
-    if(computerScore >= matchEnd){
-        result.innerHTML = "You Lose, Computer beats you";
-        return;
-    }
+function reset() {
+    userScore = 0;
+    computerScore = 0;
+    userScore_span.textContent = userScore;
+    computerScore_span.textContent = computerScore;
+    result.textContent = "Click Rock, Paper or Scissors";
 }
-// playRound();
-
